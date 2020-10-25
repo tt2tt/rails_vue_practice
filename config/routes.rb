a418c7796f7ef67d50cc4b29f0ddbd4fe33f9f95
”taro <groveleffect@gmail.com>
2020-10-26 08:37:42 +0900

    Add Books Update(API)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   namespace :api do
-    resources :books, only: [:index, :show,:create]
+    resources :books, only: [:index, :show,:create,:update]
   end
 end
