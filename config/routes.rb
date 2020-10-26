0f4db7c3f55225899aa3f9784d5fde3f216cd404
”taro <groveleffect@gmail.com>
2020-10-26 12:11:50 +0900

    Add Books Delete(API)

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   namespace :api do
-    resources :books, only: [:index, :show,:create,:update]
+    resources :books, only: [:index, :show,:create,:update, :destroy]
   end
 end
